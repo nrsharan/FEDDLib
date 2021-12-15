cb8f32f9d2578a2797e239b0948806b696ea9083
lea.sassmannshausen <lea.sassmannshausen@hotmail.de>
2021-12-15 20:54:55 +0100

    NewCase

--- meshes/Msh2Mesh/main_convertMshToMesh.m
+++ meshes/Msh2Mesh/main_convertMshToMesh.m
@@ -25,7 +25,7 @@ fprintf('>>>> Start of script.\n')
 
 %% User defined settings
 % Filename of the Gmsh-MSH-file without the extension.
-filename = 'Rohr2_8';
+filename = 'Rohrhalb';
 
 %% read meshfile
 [x,~,cElements] = MSH_Gmsh__readFile([filename '.msh']);
