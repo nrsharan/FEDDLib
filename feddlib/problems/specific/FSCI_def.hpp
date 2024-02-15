e85e506155deb9f5f19ed7971c64536b2ba5fe49
Lea <lea.sassmannshausen@hotmail.de>
2024-02-15 09:44:58 +0100

    Writing Residuals

--- feddlib/problems/specific/FSCI_def.hpp
+++ feddlib/problems/specific/FSCI_def.hpp
@@ -644,7 +644,7 @@ void FSCI<SC,LO,GO,NO>::calculateNonLinResidualVec(std::string type, double time
 
     bool plotResVector = this->getParameterList()->sublist("General").get("Plot Residual Vector",true);
     double range1 = this->getParameterList()->sublist("General").get("Plot Residual Vector Start",0.5);
-    double range2 = this->getParameterList()->sublist("General").get("Plot Residual Vector End",1.0;
+    double range2 = this->getParameterList()->sublist("General").get("Plot Residual Vector End",1.0);
     if(plotResVector && this->timeSteppingTool_->currentTime() >= range1 && this->timeSteppingTool_->currentTime() <= range2)
         this->plotResidualVec(this->timeSteppingTool_->currentTime());
 
