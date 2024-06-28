cb6988d287cc355c41a4362c0833720b1e169096
Sharan Nurani Ramesh <sharan.nuraniramesh@ruhr-uni-bochum.de>
2024-06-28 15:23:41 +0200

    6_region_artery and gitignore

--- feddlib/problems/examples/artery_6_region/main.cpp
+++ feddlib/problems/examples/artery_6_region/main.cpp
@@ -17,6 +17,8 @@ typedef int LO;
 typedef default_go GO;
 typedef Tpetra::KokkosClassic::DefaultNode::DefaultNodeType NO;
 
+void reactionTerm(double *, double *, double *);
+
 int main(int argc, char *argv[])
 {
     Teuchos::oblackhomestream blackhole;
@@ -69,27 +71,80 @@ int main(int argc, char *argv[])
 
         allParameters->setParameters(*materialParameters);
 
-        Teuchos::RCP<Teuchos::ParameterList> allChemistryParameters = Teuchos::rcp(new Teuchos::ParameterList(*chemistryPreconditionerParameters));
-        allChemistryParameters->sublist("Parameter")->setParameters(simulationParameters->sublist("Parameter Chem"));
-        allChemistryParameters->sublist("Parameter")->setParameters(simulationParameters->sublist("Simulation Parameters"));
-        allChemistryParameters->setParameters(*solverParameters);
-        allChemistryParameters->setParameters(*chemistryPreconditionerParameters);
+        Teuchos::RCP<Teuchos::ParameterList> allDiffusionParameters = Teuchos::rcp(new Teuchos::ParameterList(*chemistryPreconditionerParameters));
+        allDiffusionParameters->sublist("Parameter")->setParameters(simulationParameters->sublist("Parameter Chem"));
+        allDiffusionParameters->sublist("Parameter")->setParameters(simulationParameters->sublist("Simulation Parameters"));
+        allDiffusionParameters->setParameters(*solverParameters);
+        allDiffusionParameters->setParameters(*chemistryPreconditionerParameters);
 
         Teuchos::RCP<Teuchos::ParameterList> allStructureParameters = Teuchos::rcp(new Teuchos::ParameterList(*structurePreconditionerParameters));
         structurePreconditionerParameters->sublist("Parameter")->setParameters(simulationParameters->sublist("Parameter Solid"));
 
-        Teuchos::RCP<FEDDLib::Domain<SC,LO,GO,NO>> domainP1Diffusion;
-        Teuchos::RCP<FEDDLib::Domain<SC,LO,GO,NO>> domainP1Structure;
-        Teuchos::RCP<FEDDLib::Domain<SC,LO,GO,NO>> domainP2Diffusion;
-        Teuchos::RCP<FEDDLib::Domain<SC,LO,GO,NO>> domainP2Structure;
+        Teuchos::RCP<FEDD::Domain<SC, LO, GO, NO>> domainP1Diffusion;
+        Teuchos::RCP<FEDD::Domain<SC, LO, GO, NO>> domainP1Structure;
+        Teuchos::RCP<FEDD::Domain<SC, LO, GO, NO>> domainP2Diffusion;
+        Teuchos::RCP<FEDD::Domain<SC, LO, GO, NO>> domainP2Structure;
+        Teuchos::RCP<FEDD::Domain<SC, LO, GO, NO>> domainDiffusion;
+        Teuchos::RCP<FEDD::Domain<SC, LO, GO, NO>> domainStructure;
 
-        Teuchos::RCP<FEDDLib::Domain<SC,LO,GO,NO>> domainDiffusion;
-        Teuchos::RCP<FEDDLib::Domain<SC,LO,GO,NO>> domainStructure;
+        domainP1Diffusion.reset(new FEDD::Domain<SC, LO, GO, NO>(comm, dimension));
+        domainP1Structure.reset(new FEDD::Domain<SC, LO, GO, NO>(comm, dimension));
+        domainP2Diffusion.reset(new FEDD::Domain<SC, LO, GO, NO>(comm, dimension));
+        domainP2Structure.reset(new FEDD::Domain<SC, LO, GO, NO>(comm, dimension));
 
+        std::vector<Teuchos::RCP<FEDD::Domain<SC, LO, GO, NO>>> domainP1Array(1);
 
+        domainP1Array[0] = domainP1Structure;
 
+        Teuchos::RCP<Teuchos::ParameterList> partitionerParameters = sublist(allParameters, "Mesh Partitioner");
+        partitionerParameters->set("Build Edge List", true);
+        partitionerParameters->set("Build Surface List", true);
 
-        
+        // Read the given mesh and partition it
+        FEDD::MeshPartitioner<SC, LO, GO, NO> p1Partitioner(domainP1Array, partitionerParameters, "P1", dimension);
+        p1Partitioner.readAndPartition(15);
 
+        // Paraview export of node and element flags
+        domainP1Structure->exportElementFlags();
+        domainP1Structure->exportNodeFlags();
+
+        // Build a P2 mesh using the P1 mesh
+        domainDiffusion->buildP2ofP1Domain(domainP1Structure);
+        domainStructure->buildP2ofP1Domain(domainP1Structure);
+
+        // Set the degrees of freedem per node
+        domainStructure->setDofs(dimension);
+        domainDiffusion->setDofs(1);
+
+        // Setting the reference configuration for both domains
+        domainStructure->setReferenceConfiguration();
+        domainDiffusion->setReferenceConfiguration();
+
+        // Define the block system
+        Teuchos::RCP<FEDD::SmallMatrix<int>> defTS;
+        (*defTS)[0][0] = 1; // Structure
+        (*defTS)[1][1] = 1; // Diffusion
+
+        // Creating an object of the SCI system
+        FEDD::SCI<SC, LO, GO, NO> sci(domainStructure, discretizationType,
+                                      domainDiffusion, discretizationType,
+                                      reactionTerm, allStructureParameters,
+                                      allDiffusionParameters, allParameters,
+                                      defTS);
+        // Printing information
+        sci.info();
+
+        Teuchos::RCP<FEDD::BCBuilder<SC, LO, GO, NO>> bcFactory(new FEDD::BCBuilder<SC, LO, GO, NO>());
+        Teuchos::RCP<FEDD::BCBuilder<SC, LO, GO, NO>> bcFactoryDiffusion(new FEDD::BCBuilder<SC, LO, GO, NO>());
+        Teuchos::RCP<FEDD::BCBuilder<SC, LO, GO, NO>> bcFactoryStructure(new FEDD::BCBuilder<SC, LO, GO, NO>());
+
+        // @TODO Set boundary conditions for plaque
     }
+}
+
+// @brief Reaction Term in the reaction diffusion equation
+void reactionTerm(double *x, double *res, double *parameters)
+{
+    double m = 0.0;
+    res[0] = m * x[0];
 }
