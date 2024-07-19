9f5b84ec4347d634a5c00fa13e3992206184c584
Sharan Nurani Ramesh <sharan.nuraniramesh@ruhr-uni-bochum.de>
2024-07-19 15:35:36 +0200

    Updated simulation parameters

--- feddlib/problems/examples/artery_6_region/slurm-script.sh
+++ feddlib/problems/examples/artery_6_region/slurm-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #
-#SBATCH --job-name=sci_test
-#SBATCH --comment="sci_test"
+#SBATCH --job-name=artery_kim_guzman
+#SBATCH --comment="Artery_kim_guzman"
 #SBATCH --time=24:00:00         ### time the job will appr. run
 #SBATCH --nodes=5               ### Node count required for the job
-#SBATCH --output=/home/hpc/k105be/k105be13/slurm/outputs/sci_test.%j.out              ### output file for console output
-#SBATCH --error=/home/hpc/k105be/k105be13/slurm/outputs/sci_test.%j.err               ### output file for console error
+#SBATCH --output=/home/hpc/k105be/k105be13/slurm/outputs/kim_guzman_artery.%j.out              ### output file for console output
+#SBATCH --error=/home/hpc/k105be/k105be13/slurm/outputs/kim_guzman_artery.%j.err               ### output file for console error
 #SBATCH --ntasks=360              ### Number of tasks per job    (or next line, should one of them)
 
 unset SLURM_EXPORT_ENV
@@ -19,4 +19,4 @@ module load intel/2023.2.1 intelmpi/2021.10.0 mkl/2023.2.0 hdf5/1.12.2-oneapi202
 #ctest -j 30
 #ninja install
 #srun --mpi=mpi2 ./problems_sci_Test.exe
-srun ./problems_sci_Test.exe
+srun ./problems_artery_6_region.exe
