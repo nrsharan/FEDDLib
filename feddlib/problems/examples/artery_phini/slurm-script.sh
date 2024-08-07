8a466f557c7ca7c30a6afcd95503ace9bbf14a10
Sharan Nurani Ramesh <sharan.nuraniramesh@ruhr-uni-bochum.de>
2024-08-07 13:34:34 +0200

    Seperated test arteries for parallel execution

---
+++ feddlib/problems/examples/artery_phini/slurm-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name=artery_phini
+#SBATCH --comment="Artery_phini"
+#SBATCH --time=24:00:00         ### time the job will appr. run
+#SBATCH --nodes=4               ### Node count required for the job
+#SBATCH --output=/home/hpc/k105be/k105be13/slurm/outputs/phini_artery.%j.out              ### output file for console output
+#SBATCH --error=/home/hpc/k105be/k105be13/slurm/outputs/phini_artery.%j.err               ### output file for console error
+#SBATCH --ntasks=150              ### Number of tasks per job    (or next line, should one of them)
+
+unset SLURM_EXPORT_ENV
+
+module load intel/2023.2.1 intelmpi/2021.10.0 mkl/2023.2.0
+srun ./problems_artery_phini.exe
