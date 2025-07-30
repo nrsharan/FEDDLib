da0f29dd70346f49dd185c3aaca6bb989efd0606
Sharan Nurani Ramesh <sharan.nuraniramesh@ruhr-uni-bochum.de>
2025-07-30 12:40:45 +0200

    Bugfix

--- feddlib/core/AceFemAssembly/AssembleFE_decl.hpp
+++ feddlib/core/AceFemAssembly/AssembleFE_decl.hpp
@@ -242,6 +242,9 @@ namespace FEDD {
         void setLocalHistoryUpdated(vec_dbl_Type historyUpdated);
 
         int getHistoryLength() {return historyLength_;};
+
+        virtual std::vector<std::string> getPostDataNames();
+        virtual std::map<std::string, int> getFieldNameToPosition();
     protected:
 
         /*!
