c66237a27ffa234a9d2dcd2b472fe1141b5b9bb9
Sharan Nurani Ramesh <sharan.nuraniramesh@ruhr-uni-bochum.de>
2025-11-28 13:32:12 +0100

    Moved inline function to _decl

--- feddlib/problems/Solver/DAESolverInTime_def.hpp
+++ feddlib/problems/Solver/DAESolverInTime_def.hpp
@@ -839,8 +839,7 @@ void DAESolverInTime<SC,LO,GO,NO>::advanceInTimeSCI()
     int numSegments = parameterList_->sublist("Timestepping Parameter").sublist("Timestepping Intervalls").get("Number of Segments",0);
 
  	for(int i=1; i <= numSegments; i++){
-
-        inline bool approxEqual(double a, double b, double eps = 1e-3) {return std::abs(a - b) < eps;}
+        
         double startTime = parameterList_->sublist("Timestepping Parameter").sublist("Timestepping Intervalls").sublist(std::to_string(i)).get("Start Time",-1000.0);
         TEUCHOS_TEST_FOR_EXCEPTION(approxEqual(startTime, -1000.0), std::runtime_error, "Start Time for time segment " + std::to_string(i) + " received default value and was not set properly!");
         double dtTmp = parameterList_->sublist("Timestepping Parameter").sublist("Timestepping Intervalls").sublist(std::to_string(i)).get("dt",-3586.0);
