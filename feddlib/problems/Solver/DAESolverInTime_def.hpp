375fc2122c8b357f9cc7fd02163f827fa5a166fb
Sharan Nurani Ramesh <sharan.nuraniramesh@ruhr-uni-bochum.de>
2025-12-07 18:02:52 +0100

    Fix typo

--- feddlib/problems/Solver/DAESolverInTime_def.hpp
+++ feddlib/problems/Solver/DAESolverInTime_def.hpp
@@ -128,8 +128,8 @@ void DAESolverInTime<SC,LO,GO,NO>::setProblem(Problem_Type& problem){
 
         if(numSegments>0)
         {
-            double dtTmp = parameterList_->sublist("Timestepping Parameter").sublist("Timestepping Intervalls").sublist(std::to_string(i)).get("dt",-3586.0);
-            TEUCHOS_TEST_FOR_EXCEPTION(approxEqual(dtTmp, -3586.0), std::runtime_error, "dt for time segment " + std::to_string(i) + " received default value and was not set properly!");
+            double dtTmp = parameterList_->sublist("Timestepping Parameter").sublist("Timestepping Intervalls").sublist(std::to_string(1)).get("dt",-3586.0);
+            TEUCHOS_TEST_FOR_EXCEPTION(approxEqual(dtTmp, -3586.0), std::runtime_error, "dt for time segment " + std::to_string(1) + " received default value and was not set properly!");
             sci->timeSteppingTool_->dt_ = dtTmp; // Setting first time step size to SCI time stepping tool
             sci->timeSteppingTool_->t_ = dtTmp; // Setting initial time to first time step size (first time step)
         }
